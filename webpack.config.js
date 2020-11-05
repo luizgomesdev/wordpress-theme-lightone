09c323572c621f2252729f3f0543c10bf74b9308
Luiz Gomes <luizgomesdesenvolvimento@gmail.com>
2020-11-05 12:42:29 -0300

    Fixing modules on webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,85 +6,81 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 
 // Paths
 const BUILD_DIR = path.resolve(__dirname, 'public')
-const IMG_DIR = path.resolve(__dirname, 'src/img')
-const SCSS_DIR = path.resolve(__dirname, 'src/scss')
-const JS_DIR = path.resolve(__dirname, 'src/js')
+const IMG_DIR = path.resolve(__dirname, 'src/assets/img')
+const SCSS_DIR = path.resolve(__dirname, 'src/assets/sass')
+const JS_DIR = path.resolve(__dirname, 'src/assets/js')
 
-/**
- * Note: argv.mode will return 'development' or 'production'.
- *
- * @param argv
- */
-const plugins = argv => [
-	// Automatically remove all unused webpack assets on rebuild, when set to true in production.
-	new CleanWebpackPlugin({
-		cleanStaleWebpackAssets: argv.mode === 'production',
-	}),
-	new MiniCssExtractPlugin({
-		filename: '../css/[name].css',
-	}),
-	new BrowserSyncPlugin({
-		files: '**/*.php',
-		proxy: 'http://blog.dev.tangerino.com.br',
-	}),
-]
+module.exports = (env, argv) => {
+	return {
+		context: path.resolve(__dirname, 'src'),
 
-module.exports = (env, argv) => ({
-	context: path.resolve(__dirname, 'src'),
-
-	entry: {
-		index: [`${JS_DIR}/index.js`, `${SCSS_DIR}/index.js`],
-	},
-
-	output: {
-		path: BUILD_DIR,
-		filename: '[name].bundle.js',
-	},
-
-	module: [
-		{
-			test: /\.js$/,
-			include: [JS_DIR],
-			exclude: /node_modules/,
-			use: 'babel-loader',
+		entry: {
+			index: [`${JS_DIR}/index.js`, `${SCSS_DIR}/index.scss`],
 		},
-		{
-			test: /\.scss$/,
-			exclude: /node_modules/,
-			use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+
+		output: {
+			path: BUILD_DIR,
+			filename: 'js/[name].bundle.js',
 		},
-		{
-			test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
-			use: {
-				loader: 'file-loader',
-				options: {
-					name: '[path][name].[ext]',
-					publicPath: process.env.NODE_ENV === 'production' ? '../' : '../../',
+
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					include: [JS_DIR],
+					exclude: /node_modules/,
+					use: 'babel-loader',
 				},
-			},
-		},
-		{
-			test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-			exclude: [IMG_DIR, /node_modules/],
-			use: {
-				loader: 'file-loader',
-				options: {
-					name: '[path][name].[ext]',
-					publicPath: process.env.NODE_ENV === 'production' ? '../' : '../../',
+				{
+					test: /\.scss$/,
+					exclude: /node_modules/,
+					use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
 				},
-			},
+				{
+					test: /\.(png|jpg|svg|jpeg|gif|ico)$/,
+					use: {
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+							publicPath: argv.mode === 'production' ? '../' : '../../',
+						},
+					},
+				},
+				{
+					test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+					exclude: [IMG_DIR, /node_modules/],
+					use: {
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+							publicPath: argv.mode === 'production' ? '../' : '../../',
+						},
+					},
+				},
+			],
 		},
-	],
 
-	devtool: 'source-map',
+		devtool: 'source-map',
 
-	plugins: plugins(argv),
+		plugins: [
+			new CleanWebpackPlugin({
+				cleanStaleWebpackAssets: argv.mode === 'production',
+			}),
+			new MiniCssExtractPlugin({
+				filename: 'css/[name].css',
+			}),
+			new BrowserSyncPlugin({
+				files: '**/*.php',
+				proxy: 'http://blog.dev.tangerino.com.br',
+			}),
+		],
 
-	externals: {
-		jquery: 'jQuery',
-	},
+		externals: {
+			jquery: 'jQuery',
+		},
 
-	optimization: {
-		minimizer: [new UglifyJsPlugin({ cache: false, parallel: true, sourceMap: false })],
-	},
-})
+		optimization: {
+			minimizer: [new UglifyJsPlugin({ cache: false, parallel: true, sourceMap: false })],
+		},
+	}
+}
